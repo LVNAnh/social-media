38c69f24cf9dc5bdd7e0d9fc0c04d74bd826d613
LVNAnh <levunhatanh1997@gmail.com>
2024-06-08 17:39:24 +0700

    init vcl

--- src/routers/user/index.js
+++ src/routers/user/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userModel = require("../../models/user.model");
-
-nhu cai cc haha
+const userModel = require("../../models/user.model");
 // Create User
 router.post("/create-user", (req, res) => {
   const { name, email, password } = req.body;
