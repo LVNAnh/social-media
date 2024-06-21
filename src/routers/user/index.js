2dcbc118203fef0fdef29680ba147371c05bb068
LVNAnh <levunhatanh1997@gmail.com>
2024-06-21 12:20:30 +0700

    xoa nguoi dung va bai viet

--- src/routers/user/index.js
+++ src/routers/user/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userModel = require("../../models/user.model");
+const postModel = require("../../models/post.model");
 
 // Create User
 router.post("/create-user", (req, res) => {
@@ -68,19 +69,29 @@ router.delete("/delete-user/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
-    // Tìm người dùng và xóa
-    const deletedUser = await userModel.findByIdAndDelete(id);
+    // Find the user
+    console.log("Finding user with ID:", id);
+    const user = await userModel.findById(id);
 
-    if (!deletedUser) {
+    if (!user) {
       return res.status(404).send({ error: "User not found" });
     }
 
-    // Xóa tất cả các bài đăng của người dùng
+    // Delete all posts associated with the user
+    console.log("Deleting posts for user ID:", id);
     await postModel.deleteMany({ author: id });
 
-    res.send("User and associated posts deleted successfully");
+    // Verify posts are deleted
+    const remainingPosts = await postModel.find({ author: id });
+    console.log("Remaining posts for user ID:", id, remainingPosts);
+
+    // Delete the user
+    console.log("Deleting user with ID:", id);
+    await userModel.findByIdAndDelete(id);
+
+    res.send("Người dùng và các bài đăng liên quan đã bị xóa");
   } catch (error) {
-    console.error("Error deleting user:", error);
+    console.error("Lỗi khi xóa người dùng:", error);
     res.status(500).send(error);
   }
 });
